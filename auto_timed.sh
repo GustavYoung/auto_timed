1b9a469ad971558ef56ed1f437e028698ae1c5b0
Gustav_Young <gus.santana@icloud.com>
2018-11-24 14:50:37 -0600

    rc1_avT_GOOGL_403
    Primera versión que se aprovecha de cómo devuelven los errores los servidores HTML par obtener la hora actual.

---
+++ auto_timed.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#Copyright 2018 Gustavo Santana
+#(C) Mirai Works LLC
+#Desactivamos el puto cursor >P
+sleep 900;
+
+# Nombre de instancia para que no choque con la de uxmalstream
+SERVICE="Hora_auto_1";
+
+# infinite loop!
+while true; do
+        if ps ax | grep -v grep | grep $SERVICE > /dev/null
+        then
+        sleep 1;
+else
+        sudo date -s "$(wget -qSO- --max-redirect=0 google.com 2>&1 | grep Date: | cut -d' ' -f5-8)Z";
+        eleep 5400;
+        done
+fi
+done
